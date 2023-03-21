f79a259aa8d87d5bafcd4c9b9702a84b86196e6c
Jacob Alarcon <jacobalarcon@gmail.com>
2023-03-21 15:33:54 -0500

    practice complete

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.get('/home', (req, res) => {
-  res.status(200).json({ message: 'hello from from get request land' });
+  res.status(200).sendFile(path.join(__dirname, '..', 'src', 'index.html'));
 });
 
 app.listen(PORT, () => {
